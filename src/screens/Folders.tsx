09a92eabd33d54a7bff5efab9d58edce40d5435a
MuazzezA <ayd.muazzez@gmail.com>
2024-03-31 17:04:59 +0300

    feat: project main files

---
+++ src/screens/Folders.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const Folders = () => {
+  return (
+    <View>
+      <Text>FOLDERS</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+export default Folders;
