3da751a8c4edc0dee1ee4ef90fbc518161f61752
MuazzezA <ayd.muazzez@gmail.com>
2024-04-07 03:02:48 +0300

    feat: env setting

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'module:react-native-dotenv',
+      {
+        envName: 'APP_ENV',
+        moduleName: '@env',
+        path: '.env',
+      },
+    ],
+  ],
   env: {
     production: {
       plugins: ['react-native-paper/babel'],
