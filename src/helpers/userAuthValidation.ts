cfb4eac2c566952440deffb63221b5fd53f5008d
MuazzezA <ayd.muazzez@gmail.com>
2024-04-07 03:02:18 +0300

    feat: google flow functions

---
+++ src/helpers/userAuthValidation.ts
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import {getUserSession} from '../store/storage.ts';
+
+const userAuthValidation = async () => {
+  try {
+    const data = await getUserSession();
+    if (!data) {
+      return {isValid: false};
+    }
+    const accessToken = JSON.parse(data).token;
+    const response = await axios.get(
+      `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`,
+    );
+    console.log('Token information:', response.data);
+    return {isValid: true, data: response.data};
+  } catch (error) {
+    console.error('Error validating token:', error);
+    return {isValid: false, error: error};
+  }
+};
+
+export default userAuthValidation;
---
+++ src/store/storage.ts
@@ -0,0 +1,58 @@
+import EncryptedStorage from 'react-native-encrypted-storage';
+import {USER_ACCESS_TOKEN, USER_DATA} from '../constants/General.ts';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+export const setUserSession = async (token: string) => {
+  try {
+    await EncryptedStorage.setItem(
+      USER_ACCESS_TOKEN,
+      JSON.stringify({
+        token: token,
+      }),
+    );
+  } catch (error) {
+    console.error('ERROR setUserSession : ', error);
+  }
+};
+
+export const getUserSession = async () => {
+  try {
+    const session = await EncryptedStorage.getItem(USER_ACCESS_TOKEN);
+    return session ? session : '';
+  } catch (error) {
+    console.error('ERROR getUserSession : ', error);
+  }
+};
+
+export const clearUserSession = async () => {
+  try {
+    await EncryptedStorage.clear();
+  } catch (e) {
+    console.error('ERROR clearUserSession : ', e);
+  }
+};
+
+export const storeUserData = async (value: {}) => {
+  try {
+    const jsonValue = JSON.stringify(value);
+    await AsyncStorage.setItem(USER_DATA, jsonValue);
+  } catch (error) {
+    console.error('ERROR storeUserData : ', error);
+  }
+};
+
+export const getUserData = async () => {
+  try {
+    const jsonValue = await AsyncStorage.getItem(USER_DATA);
+    return jsonValue != null ? JSON.parse(jsonValue) : null;
+  } catch (error) {
+    console.error('ERROR getUserData : ', error);
+  }
+};
+
+export const clearStorage = async () => {
+  try {
+    await AsyncStorage.clear();
+  } catch (e) {
+    console.error('ERROR clearStorage : ', e);
+  }
+};
