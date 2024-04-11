c82fa3f70698d250ad22523e6588f5485070a372
MuazzezA <ayd.muazzez@gmail.com>
2024-04-11 16:44:22 +0300

    feat: home and login ui

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,10 +1,12 @@
 import React, {useEffect} from 'react';
-import {StyleSheet, Text} from 'react-native';
+import {Dimensions, Image, StyleSheet, Text} from 'react-native';
 import {GoogleSigninButton} from '@react-native-google-signin/google-signin';
 import {useSignInGoogle} from '../hooks/useSignInGoogle.ts';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {ScreenName} from '../navigation/ScreenName.ts';
-
+import GText from '../components/GText.tsx';
+import {colors} from '../constants/Theme.ts';
+const {width, height} = Dimensions.get('window');
 // @ts-ignore
 const Login = ({navigation}) => {
   const {signIn, loading, isSuccess} = useSignInGoogle();
@@ -15,7 +17,16 @@ const Login = ({navigation}) => {
   }, [isSuccess, navigation]);
   return (
     <SafeAreaView style={styles.screen}>
-      <Text>WELCOME</Text>
+      <Image
+        source={require('../assets/icons/email-2.png')}
+        style={styles.image}
+      />
+      <GText
+        size={20}
+        text={'GMAIL MANAGER'}
+        weight={'bold'}
+        color={colors.primary}
+      />
       <GoogleSigninButton onPress={signIn} disabled={loading} />
     </SafeAreaView>
   );
@@ -26,8 +37,16 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    paddingBottom: 120,
+    paddingTop: width * 0.7,
     gap: 20,
+  },
+  image: {
+    resizeMode: 'contain',
+    opacity: 0.3,
+    position: 'absolute',
+    width: width * 1.5,
+    left: 0,
+    top: height * 0.1,
   },
 });
 export default Login;
