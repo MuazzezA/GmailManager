2f931147bc48912770cfe3ab2983b25668939912
MuazzezA <ayd.muazzez@gmail.com>
2024-04-13 18:15:54 +0300

    fix: undefined check

--- src/helpers/getEmailSubject.ts
+++ src/helpers/getEmailSubject.ts
@@ -1,6 +1,10 @@
 import {MailType} from '../types/MailType.ts';
 
 export const getEmailSubject = (mail: MailType) => {
+  if (!mail) {
+    return '';
+  }
   const headers = mail.payload.headers;
-  return headers.find(h => h.name === 'Subject');
+  const subject = headers.find(h => h.name === 'Subject');
+  return subject?.value;
 };
