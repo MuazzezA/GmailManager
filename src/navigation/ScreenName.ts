9529214874ee38955323f969f73023fab77b81c8
MuazzezA <ayd.muazzez@gmail.com>
2024-05-17 22:19:33 +0300

    fix: update route type

--- src/navigation/ScreenName.ts
+++ src/navigation/ScreenName.ts
@@ -1,17 +1,19 @@
-export const ScreenName: {
-  FOLDERS: string;
-  TABS: string;
-  SPLASH: string;
-  LOGIN: string;
-  SETTINGS: string;
-  DETAIL: string;
-  HOME: string;
-} = {
-  SPLASH: 'splash',
-  TABS: 'tabs',
-  HOME: 'home',
-  LOGIN: 'login',
-  FOLDERS: 'folders',
-  SETTINGS: 'settings',
-  DETAIL: 'detail',
+export enum ScreenName {
+  SPLASH = 'splash',
+  TABS = 'tabs',
+  HOME = 'home',
+  LOGIN = 'login',
+  FOLDERS = 'folders',
+  SETTINGS = 'settings',
+  DETAIL = 'detail',
+}
+
+export type RootStackParamList = {
+  [ScreenName.SPLASH]: undefined;
+  [ScreenName.TABS]: undefined;
+  [ScreenName.HOME]: undefined;
+  [ScreenName.LOGIN]: undefined;
+  [ScreenName.FOLDERS]: undefined;
+  [ScreenName.SETTINGS]: undefined;
+  [ScreenName.DETAIL]: {mail: string};
 };
