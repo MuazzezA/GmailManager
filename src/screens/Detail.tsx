c79da88e31170f8af833b3fdbdffb5b941f74b05
MuazzezA <ayd.muazzez@gmail.com>
2024-05-17 22:19:18 +0300

    feat: detail screen mail body

--- src/screens/Detail.tsx
+++ src/screens/Detail.tsx
@@ -1,19 +1,35 @@
-import React from 'react';
-import {StyleSheet, Text, SafeAreaView} from 'react-native';
-import {useRoute} from '@react-navigation/native';
+import React, {useEffect, useLayoutEffect, useState} from 'react';
+import {StyleSheet, SafeAreaView} from 'react-native';
+import {RouteProp, useNavigation, useRoute} from '@react-navigation/native';
 import {ScreenName} from '../navigation/ScreenName.ts';
 import {getEmailSender} from '../helpers/getEmailSender.ts';
-import {getEmailSubject} from '../helpers/getEmailSubject.ts';
+import WebView from 'react-native-webview';
+import {getMailBody} from '../helpers/getMailBody.tsx';
+import {MailType} from '../types/MailType.ts';
 
 const Detail = () => {
-  const route = useRoute<ScreenName.DETAIL>();
-  const mail = route.params?.mail;
+  const navigation = useNavigation();
+  const route = useRoute<RouteProp<ScreenName.DETAIL>>();
+  const mail = route.params?.mail as MailType;
   const sender = getEmailSender(mail);
-  const subject = getEmailSubject(mail);
+
+  useLayoutEffect(() => {
+    const title =
+      sender?.length > 20 ? `${sender.substring(0, 25)}...` : sender;
+    navigation.setOptions({
+      headerTitle: title,
+    });
+  }, []);
+
   return (
     <SafeAreaView style={styles.screen}>
-      <Text style={styles.senderText}>{sender}</Text>
-      <Text style={styles.subjectText}>{subject}</Text>
+      <WebView
+        showsVerticalScrollIndicator={false}
+        originWhitelist={['*']}
+        bounces={false}
+        source={{html: getMailBody(mail)}}
+        style={{flex: 1}}
+      />
     </SafeAreaView>
   );
 };
@@ -23,14 +39,7 @@ const styles = StyleSheet.create({
     gap: 8,
     flex: 1,
     marginHorizontal: 8,
-  },
-  senderText: {
-    fontSize: 22,
-    fontWeight: '700',
-  },
-  subjectText: {
-    fontSize: 18,
-    fontWeight: '300',
+    backgroundColor: 'white',
   },
 });
 export default Detail;
