03db4905a755032aa012fef4e445f91454df63d0
MuazzezA <ayd.muazzez@gmail.com>
2024-05-19 11:34:03 +0300

    fix: store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,18 @@
-import {create} from 'zustand';
+import {createJSONStorage, persist} from 'zustand/middleware';
+import {createWithEqualityFn} from 'zustand/traditional';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export const useAppStore = create(set => ({
-  folders: [],
-  setFolders: (data: []) => set({folders: data}),
-}));
+export const useAppStore = createWithEqualityFn(
+  persist(
+    set => ({
+      folders: null,
+      setFolders: data => set({folders: data}),
+      savedMails: null,
+      setSavedMails: data => set({savedMails: data}),
+    }),
+    {
+      name: 'app-storage',
+      storage: createJSONStorage(() => AsyncStorage),
+    },
+  ),
+);
