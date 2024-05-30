3c193fb15da9e264c249fdf82dca13197ecb7426
MuazzezA <ayd.muazzez@gmail.com>
2024-05-30 22:59:05 +0300

    fix: create folder method

--- src/hooks/useFolders.ts
+++ src/hooks/useFolders.ts
@@ -3,10 +3,12 @@ import {shallow} from 'zustand/shallow';
 import {FolderType} from '../types/FolderType.ts';
 
 export const useFolders = () => {
-  const {setFolders, folders} = useAppStore(
+  const {setFolders, folders, setSavedMails, savedMails} = useAppStore(
     state => ({
       setFolders: state.setFolders,
       folders: state.folders,
+      setSavedMails: state.setSavedMails,
+      savedMails: state.savedMails,
     }),
     shallow,
   );
@@ -21,23 +23,38 @@ export const useFolders = () => {
     createAfter: () => void;
   }) => {
     if (folderName.length > 3 && folderSubject.length > 5) {
-      const newList = folders
-        ? [
-            ...folders,
-            {
-              folderName,
-              folderSubject,
-              id: folders.length + 1,
-            },
-          ]
-        : [
-            {
-              folderName,
-              folderSubject,
-              id: 0,
-            },
-          ];
-      setFolders(newList);
+      if (folders?.length > 0) {
+        setFolders([
+          ...folders,
+          {
+            folderName,
+            folderSubject,
+            id: folders.length,
+          },
+        ]);
+        setSavedMails([
+          ...savedMails,
+          {
+            id: folders.length,
+            mails: [],
+          },
+        ]);
+      } else {
+        setFolders([
+          {
+            folderName,
+            folderSubject,
+            id: 0,
+          },
+        ]);
+        setSavedMails([
+          {
+            id: 0,
+            mails: [],
+          },
+        ]);
+      }
+
       createAfter?.();
     }
   };
