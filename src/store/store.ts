8df5806717b74be43bf31d3f4c4c199deeb65945
MuazzezA <ayd.muazzez@gmail.com>
2024-06-03 22:01:12 +0300

    fix: store type

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,20 +1,30 @@
 import {createJSONStorage, persist} from 'zustand/middleware';
-import {createWithEqualityFn} from 'zustand/traditional';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {FolderType} from '../types/FolderType.ts';
 import {MailType} from '../types/MailType.ts';
+import {StateCreator} from 'zustand';
+import {createWithEqualityFn} from 'zustand/traditional';
 
-export const useAppStore = createWithEqualityFn(
-  persist(
-    set => ({
-      folders: null,
-      setFolders: (data: FolderType) => set({folders: data}),
-      savedMails: null,
-      setSavedMails: (data: MailType | MailType[]) => set({savedMails: data}),
-    }),
-    {
-      name: 'app-storage',
-      storage: createJSONStorage(() => AsyncStorage),
-    },
-  ),
+type AppStateType = {
+  folders: FolderType | null;
+  setFolders: (data: FolderType) => void;
+  savedMails: MailType | MailType[] | null;
+  setSavedMails: (data: MailType | MailType[]) => void;
+};
+
+const appStateSlice: StateCreator<
+  AppStateType,
+  [['zustand/persist', unknown]]
+> = set => ({
+  folders: null,
+  setFolders: (data: FolderType) => set({folders: data}),
+  savedMails: null,
+  setSavedMails: (data: MailType | MailType[]) => set({savedMails: data}),
+});
+
+export const useAppStore = createWithEqualityFn<AppStateType>()(
+  persist(appStateSlice, {
+    name: 'forest-storage',
+    storage: createJSONStorage(() => AsyncStorage),
+  }),
 );
