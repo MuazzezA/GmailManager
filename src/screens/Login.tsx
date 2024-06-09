7eaa959c881e5cd1575a66f4257f5f17796eb644
MuazzezA <ayd.muazzez@gmail.com>
2024-06-09 16:05:37 +0300

    fix: store folder types

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,11 +1,12 @@
 import React, {useEffect} from 'react';
-import {Dimensions, Image, StyleSheet, Text} from 'react-native';
+import {Dimensions, Image, StyleSheet, View} from 'react-native';
 import {GoogleSigninButton} from '@react-native-google-signin/google-signin';
 import {useSignInGoogle} from '../hooks/useSignInGoogle.ts';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {ScreenName} from '../navigation/ScreenName.ts';
 import GText from '../components/GText.tsx';
 import {colors} from '../constants/Theme.ts';
+
 const {width, height} = Dimensions.get('window');
 // @ts-ignore
 const Login = ({navigation}) => {
@@ -22,12 +23,36 @@ const Login = ({navigation}) => {
         style={styles.image}
       />
       <GText
-        size={20}
+        size={24}
         text={'GMAIL MANAGER'}
         weight={'bold'}
         color={colors.primary}
       />
-      <GoogleSigninButton onPress={signIn} disabled={loading} />
+      <GText
+        size={12}
+        text={
+          'The GmailManager app allows you to list your emails and organize them into folders. All data is stored only on your device and is not shared with any other person or organization. '
+        }
+        weight={'500'}
+        color={colors.text}
+        style={{
+          textAlign: 'center',
+          padding: width * 0.1,
+        }}
+      />
+      <GText
+        size={14}
+        text={'Sing In With Google'}
+        weight={'600'}
+        color={colors.text}
+        style={{
+          textAlign: 'center',
+        }}
+      />
+      <View style={styles.shadow}>
+        <GoogleSigninButton onPress={signIn} disabled={loading} />
+      </View>
+      <View></View>
     </SafeAreaView>
   );
 };
@@ -37,8 +62,8 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    paddingTop: width * 0.7,
-    gap: 20,
+    paddingTop: height * 0.1,
+    gap: 22,
   },
   image: {
     resizeMode: 'contain',
@@ -47,6 +72,16 @@ const styles = StyleSheet.create({
     width: width * 1.5,
     left: 0,
     top: height * 0.1,
+  },
+  shadow: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4.65,
+    elevation: 8,
   },
 });
 export default Login;
