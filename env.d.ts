2c8dde73d7d241f779c72c99ee14acbd5ea9aa17
MuazzezA <ayd.muazzez@gmail.com>
2024-06-09 16:07:47 +0300

    fix: app json

--- env.d.ts
+++ env.d.ts
@@ -1,4 +1,6 @@
 declare module '@env' {
   export const WEB_CLIENT_ID: string;
   export const IOS_CLIENT_ID: string;
+  export const DEFAULT_BANNER: string;
+  export const DEFAULT_INTERSTITIAL_BANNER: string;
 }
