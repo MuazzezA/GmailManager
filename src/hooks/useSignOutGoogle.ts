cfc757d0ac244264ee58efb2f19d65c6cbe35c84
MuazzezA <ayd.muazzez@gmail.com>
2024-06-11 19:20:13 +0300

    feat: sign out base

---
+++ src/hooks/useSignOutGoogle.ts
@@ -0,0 +1,22 @@
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
+import {useState} from 'react';
+import {clearStorage, clearUserSession} from '../store/logic.ts';
+
+export const useSignOutGoogle = () => {
+  const [loading, setLoading] = useState(false);
+
+  const signIn = async () => {
+    setLoading(true);
+    try {
+      await GoogleSignin.signOut();
+      await clearUserSession();
+      await clearStorage();
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return {loading, signIn};
+};
--- src/screens/Settings.tsx
+++ src/screens/Settings.tsx
@@ -1,13 +1,126 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useCallback, useEffect, useState} from 'react';
+import {
+  ActivityIndicator,
+  Alert,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import {clearStorage, clearUserSession, getUserData} from '../store/logic.ts';
+import GText from '../components/GText.tsx';
+import {ScrollView} from 'react-native-gesture-handler';
+import {colors} from '../constants/Theme.ts';
 
 const Settings = () => {
+  const [user, setUser] = useState();
+
+  const userData = useCallback(async () => {
+    const data = await getUserData();
+    setUser(data.user);
+    console.log(data.user);
+  }, []);
+
+  useEffect(() => {
+    userData().then();
+  }, []);
+
+  const deleteData = () => {
+    clearUserSession().then();
+    clearStorage().then();
+  };
+
+  const signOut = () => {};
+
+  if (!user) {
+    return <ActivityIndicator style={{flex: 1}} />;
+  }
+
   return (
-    <View>
-      <Text>Settings</Text>
-    </View>
+    <ScrollView contentContainerStyle={styles.screen}>
+      <View style={styles.row}>
+        <View style={{gap: 4}}>
+          <GText text={user?.email} size={18} style={styles.text} />
+          <GText text={user?.name} size={16} />
+        </View>
+        <Image source={{uri: user?.photo}} style={styles.image} />
+      </View>
+
+      <TouchableOpacity
+        style={styles.button}
+        activeOpacity={0.8}
+        onPress={() => {
+          Alert.alert('Warning', 'The folders you created will be deleted.', [
+            {
+              style: 'cancel',
+              text: 'Cancel',
+              onPress: () => {},
+            },
+            {
+              text: 'Delete',
+              onPress: () => signOut(),
+              style: 'destructive',
+            },
+          ]);
+        }}>
+        <GText text={'Delete All My Data'} size={16} style={styles.text} />
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          deleteData();
+          Alert.alert('Warning', 'The folders you created will be deleted.', [
+            {
+              style: 'cancel',
+              text: 'Cancel',
+              onPress: () => {},
+            },
+            {
+              text: 'Delete',
+              onPress: () => deleteData(),
+              style: 'destructive',
+            },
+          ]);
+          // sign out
+        }}
+        style={[styles.button, {backgroundColor: colors.primary}]}
+        activeOpacity={0.8}>
+        <GText
+          text={'Sing Out'}
+          size={16}
+          style={styles.text}
+          color={'white'}
+        />
+      </TouchableOpacity>
+    </ScrollView>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    height: 75,
+    width: 75,
+    borderRadius: 45,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 12,
+    paddingVertical: 16,
+    flexWrap: 'wrap',
+    gap: 4,
+  },
+  screen: {
+    flex: 1,
+  },
+  text: {
+    fontFamily: 'Quicksand-Bold',
+  },
+  button: {
+    margin: 8,
+    padding: 12,
+    backgroundColor: colors.secondary,
+    borderRadius: 8,
+  },
+});
 export default Settings;
