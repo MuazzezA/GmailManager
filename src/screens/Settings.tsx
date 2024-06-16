07d6e3b439aa564080297513b852002973741192
MuazzezA <ayd.muazzez@gmail.com>
2024-06-16 22:17:59 +0300

    fix: clear storage

--- src/screens/Settings.tsx
+++ src/screens/Settings.tsx
@@ -2,10 +2,8 @@ import React, {useCallback, useEffect, useState} from 'react';
 import {
   ActivityIndicator,
   Alert,
-  Image,
   StyleSheet,
   TouchableOpacity,
-  View,
 } from 'react-native';
 import {clearStorage, clearUserSession, getUserData} from '../store/logic.ts';
 import GText from '../components/GText.tsx';
@@ -15,11 +13,17 @@ import {useSignOutGoogle} from '../hooks/useSignOutGoogle.ts';
 import {useNavigation} from '@react-navigation/native';
 import {ScreenName} from '../navigation/ScreenName.ts';
 import {User} from '../types/User.ts';
+import {useAppStore} from '../store/store.ts';
 
 const Settings = () => {
   const navigation = useNavigation();
   const [user, setUser] = useState<User | null | undefined>();
   const {loading, signOut} = useSignOutGoogle();
+
+  const {setFolders, setSavedMails} = useAppStore(state => ({
+    setFolders: state.setFolders,
+    setSavedMails: state.setSavedMails,
+  }));
 
   const userData = useCallback(async () => {
     const data = await getUserData();
@@ -33,57 +37,62 @@ const Settings = () => {
   const deleteData = () => {
     clearUserSession().then().catch();
     clearStorage().then().catch();
+    setFolders([]);
+    setSavedMails([]);
+    // todo : resetin zustand persist şeklini araştır
   };
 
   if (!user || loading) {
     return <ActivityIndicator style={styles.screen} />;
   }
 
+  const deleteAction = () => {
+    Alert.alert('Warning', 'The folders you created will be deleted.', [
+      {
+        style: 'cancel',
+        text: 'Cancel',
+        onPress: () => {},
+      },
+      {
+        text: 'Delete',
+        onPress: () => deleteData(),
+        style: 'destructive',
+      },
+    ]);
+  };
+  const signOutAction = () => {
+    Alert.alert('Warning', 'Sign Out', [
+      {
+        style: 'cancel',
+        text: 'Cancel',
+        onPress: () => {},
+      },
+      {
+        text: 'Sign Out',
+        onPress: () => signOut(),
+        style: 'destructive',
+      },
+    ]);
+  };
+
   return (
     <ScrollView contentContainerStyle={styles.screen}>
-      <View style={styles.row}>
+      {/*<View style={styles.row}>
         <View style={styles.gap}>
           <GText text={user?.email} size={18} style={styles.text} />
           <GText text={user?.name} size={16} />
         </View>
         <Image source={{uri: user?.photo}} style={styles.image} />
-      </View>
+      </View>*/}
 
       <TouchableOpacity
         style={styles.button}
         activeOpacity={0.8}
-        onPress={() => {
-          Alert.alert('Warning', 'The folders you created will be deleted.', [
-            {
-              style: 'cancel',
-              text: 'Cancel',
-              onPress: () => {},
-            },
-            {
-              text: 'Delete',
-              onPress: () => deleteData(),
-              style: 'destructive',
-            },
-          ]);
-        }}>
+        onPress={() => deleteAction()}>
         <GText text={'Delete All My Data'} size={16} style={styles.text} />
       </TouchableOpacity>
       <TouchableOpacity
-        onPress={() => {
-          Alert.alert('Warning', 'Sign Out', [
-            {
-              style: 'cancel',
-              text: 'Cancel',
-              onPress: () => {},
-            },
-            {
-              text: 'Delete',
-              onPress: () => signOut(),
-              style: 'destructive',
-            },
-          ]);
-          // sign out
-        }}
+        onPress={() => signOutAction()}
         style={[styles.button, {backgroundColor: colors.primary}]}
         activeOpacity={0.8}>
         <GText
