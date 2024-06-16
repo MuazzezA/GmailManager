b1b94e97a78295f6af23d28c982d9d5720f07d7f
MuazzezA <ayd.muazzez@gmail.com>
2024-06-16 22:17:59 +0300

    fix: clear storage

--- src/hooks/useSignOutGoogle.ts
+++ src/hooks/useSignOutGoogle.ts
@@ -1,9 +1,11 @@
 import {GoogleSignin} from '@react-native-google-signin/google-signin';
 import {useState} from 'react';
 import {clearStorage, clearUserSession} from '../store/logic.ts';
+import {CommonActions, useNavigation} from '@react-navigation/native';
 
 export const useSignOutGoogle = () => {
   const [loading, setLoading] = useState(false);
+  const navigation = useNavigation();
 
   const signOut = async () => {
     setLoading(true);
@@ -11,6 +13,12 @@ export const useSignOutGoogle = () => {
       await GoogleSignin.signOut();
       await clearUserSession();
       await clearStorage();
+      navigation.dispatch(
+        CommonActions.reset({
+          index: 1,
+          routes: [{name: 'login'}],
+        }),
+      );
     } catch (e) {
       console.error(e);
     } finally {
